c9e62110aa00e7e1b9593e8fbaed5b64afcf75d7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-09 21:57:34 +0100

    smallUpdate

--- src/main/java/com/raxrot/sproject/model/Category.java
+++ src/main/java/com/raxrot/sproject/model/Category.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(schema = "categories")
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
