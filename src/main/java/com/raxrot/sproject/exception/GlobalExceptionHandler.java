a29be21faa30ef671dc6287810f9032df188fa38
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-10 16:33:04 +0100

    add GlobalExceptionHandler

---
+++ src/main/java/com/raxrot/sproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.raxrot.sproject.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String,String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String,String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return new ResponseEntity<>(response,ex.getStatusCode());
+    }
+}
