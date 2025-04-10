559ef0b01b5ecf2fa3159bbbfc8f520dbaa7178f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-10 19:05:38 +0100

    remove Validation

--- src/main/java/com/raxrot/sproject/model/Category.java
+++ src/main/java/com/raxrot/sproject/model/Category.java
@@ -1,9 +1,9 @@
 package com.raxrot.sproject.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -14,7 +14,5 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Category name cannot be blank")
-    @Size(min = 3,message = "Category should contain at least 3 characters")
     private String categoryName;
 }
