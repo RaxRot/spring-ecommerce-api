d16cf50e0b09360488e09733dd974c4a0a573842
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-14 18:37:38 +0100

    add OpenApiDefinition

--- src/main/java/com/raxrot/sproject/SbEcomApplication.java
+++ src/main/java/com/raxrot/sproject/SbEcomApplication.java
@@ -1,8 +1,33 @@
 package com.raxrot.sproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:8080/swagger-ui/index.html#/
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SB E-Commerce API",
+                version = "1.0",
+                description = "Spring Boot E-Commerce REST API",
+                contact = @Contact(
+                        name = "RaxRot ❤️",
+                        email = "dasistperfektosss@gmail.com",
+                        url = "https://github.com/RaxRot"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        }
+)
 @SpringBootApplication
 public class SbEcomApplication {
 
