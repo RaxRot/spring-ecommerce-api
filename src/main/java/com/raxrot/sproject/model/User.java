23e9ca7bc38b2e77099a1393a91a2e0eb9dc9ee2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-17 14:16:56 +0100

    increase length password to 100 to avoid error

--- src/main/java/com/raxrot/sproject/model/User.java
+++ src/main/java/com/raxrot/sproject/model/User.java
@@ -33,7 +33,7 @@ public class User {
     private String email;
 
     @NotBlank
-    @Size(max = 50)
+    @Size(max = 100)
     private String password;
 
     public User(String username, String email, String password) {
