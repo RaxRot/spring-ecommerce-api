87e4d7e31df55564d58cc25fad9fee5cbbdc2815
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-21 12:45:05 +0100

    add relationships with cart/itemCart

--- src/main/java/com/raxrot/sproject/model/User.java
+++ src/main/java/com/raxrot/sproject/model/User.java
@@ -63,4 +63,8 @@ public class User {
     @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses=new ArrayList<>();
+
+    @ToString.Exclude
+    @OneToOne(mappedBy = "user",cascade = {CascadeType.PERSIST,CascadeType.MERGE},orphanRemoval = true)
+    private Cart cart;
 }
